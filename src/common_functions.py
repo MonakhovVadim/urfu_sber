9acdcc42d808cfb5c6de55c4794b2bd1196f0073
operevispa <perevispa@mail.ru>
2024-12-12 22:45:52 +0300

    correct recalculate_values functions

--- src/common_functions.py
+++ src/common_functions.py
@@ -19,110 +19,126 @@ def load_scor_model():
     Если существует файл scor_model.xlsx (сохраненный пользователем), то подгружаются эти данные
     Если файл не существует, загружаются дефолтные данные из default_scor_model.xlsx
     Возвращает:
-     - датафрейм содержищий критерии оценки, их веса, минимально и максимально 
+     - датафрейм содержищий критерии оценки, их веса, минимально и максимально
      возможные значения критерив, описания критериев
     """
-    if os.path.exists('data/scor_model.xlsx'):
-        return pd.read_excel('data/scor_model.xlsx')
-    elif os.path.exists('data/default_scor_model.xlsx'):
-        return pd.read_excel('data/default_scor_model.xlsx')
-    else:        
+    if os.path.exists("data/scor_model.xlsx"):
+        return pd.read_excel("data/scor_model.xlsx", dtype={"weight": float})
+    elif os.path.exists("data/default_scor_model.xlsx"):
+        return pd.read_excel("data/default_scor_model.xlsx")
+    else:
         return pd.DataFrame()
-    
+
 
 def save_scor_model(df):
     """
     Сохраняет новые веса критериям, заданные пользователем
     """
-    df.to_excel('data/scor_model.xlsx', index=False)
+    df.to_excel("data/scor_model.xlsx", index=False)
 
 
-def dependence_df(df, df_features):
+def recalculate_values(df_data, df_params):
     """
     Функция возвращает датафрейм с оценкой критериев, измененных с учетом direct_dependence каждого критерия
     Критерии в датафрейме могут иметь либо прямую либо обратную зависимость, между значением критерия и риском.
     Каждый критерий имеет параметр direct_dependence
     - direct_dependence = 1, означает прямую зависмость риска от значения (чем выше значение, тем выше риск)
-    - direct_dependence = 0, означает обратную зависимость, чем ниже значение, тем выше риск    
+    - direct_dependence = 0, означает обратную зависимость, чем ниже значение, тем выше риск
     Параметры:
-     - df: датафрейм с данными
-     - features_df: датафрейм с весами критериев
+     - df_data: датафрейм с данными
+     - df_params: датафрейм с параметрами критериев (вес, зависимость, максимальное значение)
      Возвращает:
-     - измененный df с учетом direct_dependence 
-    """    
-        
-    #for feature in df_features:
-    #    if feature['direct_dependence'] == 0:
-    #        df[feature['name']][] = 5 - df[feature['name']]
-    
-    
-    return df
+     - измененный df с учетом direct_dependence
+    """
+
+    # Создаем копию датафрейма с данными для изменения
+    df_recalculated = df_data.copy()
+
+    # Проходим по всем фичам в параметрах
+    for _, row in df_params.iterrows():
+        feature = row["name"]
+        direct_dependence = row["direct_dependence"]
+        max_value = row["max_value"]
+
+        # Если обратная зависимость, пересчитываем значения
+        if direct_dependence == 0:
+            df_recalculated[feature] = max_value - df_recalculated[feature]
+
+    return df_recalculated
 
 
-def normalize_df(df_features):
+def normalize_df(df_params):
     """
     Нормализует веса датафрейма с критериями
-    """    
+    """
     # совокупный вес всех критериев
-    total_weight = df_features.weight.sum()
+    total_weight = df_params.weight.sum()
     # нормализуем веса
-    df_features['weight'] = df_features['weight'] / total_weight
-    
-    return df_features
+    df_params["weight"] = df_params["weight"] / total_weight
+
+    return df_params
 
 
-def calculate_scor(df, df_features):
+def calculate_scor(df_data, df_params):
     """
     Функция математически рассчитывает и возвращает скорбал
     Параметры:
-     - df: датафрейм с введенными пользователем значениями критериев
-     - features_df: датафрейм с весами критериев
+     - df_data: датафрейм с введенными пользователем значениями критериев
+     - df_params: датафрейм с весами критериев
      Возвращает:
      - математически рассчитанный бал оценки риска релиза
-     
+
     """
     # нормализуем вес критериев
-    df_features = normalize_df(df_features)
+    df_params = normalize_df(df_params)
     # меняем оценки на противоположные для критериев с обатной зависимостью
-    df = dependence_df(df, df_features)
-   
-    return sum(df.iloc[0][feature] * df_features.loc[df_features['name'] == feature, 'weight'].values[0]
-                   for feature in df_features['name'])
+    df_data = recalculate_values(df_data, df_params)
+
+    return sum(
+        df_data.iloc[0][feature]
+        * df_params.loc[df_params["name"] == feature, "weight"].values[0]
+        for feature in df_params["name"]
+    )
 
 
-def dataset_generation(df_features, num_samples = 1000):
+def dataset_generation(df_params, num_samples=1000):
     """
     Функция генерации синтетического датасета. Таргетный скорбал рассчитывается математически,
-    на основании весов, заданных экспертом и сохраненных в файле scor_model 
+    на основании весов, заданных экспертом и сохраненных в файле scor_model
     либо default_scor_model (если веса по-умолчанию не менялись)
     Параметры:
-    - df_features: датафрейм с критериями и их весами
+    - df_params: датафрейм с критериями и их весами
     - num_samples: количесвто строк для генерации в синтетическом датасете (по умолчанию 1000)
     """
-    
+
     # если датафрейм пустой, значит у нас нет весов и мы не сможем сгенерировать датасет
-    if df_features.empty:
+    if df_params.empty:
         return -1
-    
-    df_features = normalize_df(df_features)
-    
+
+    df_params = normalize_df(df_params)
+
     # Генерация синтетических данных
-    synthetic_data = {}    
-    for index, row in df_features.iterrows():
-        feature_values = np.random.randint(row['min_value'], row['max_value']+1, num_samples)
-        synthetic_data[row['name']] = feature_values
+    synthetic_data = {}
+    for index, row in df_params.iterrows():
+        feature_values = np.random.randint(
+            row["min_value"], row["max_value"] + 1, num_samples
+        )
+        synthetic_data[row["name"]] = feature_values
 
     # Создание датафрейма из сгенерированных данных
     df_synthetic = pd.DataFrame(synthetic_data)
-    
+
     # Функция для расчета целевой переменной
     def calc_target(row):
-        return sum(row[feature] * df_features.loc[df_features['name'] == feature, 'weight'].values[0]
-                   for feature in df_features['name'])
+        return sum(
+            row[feature]
+            * df_params.loc[df_params["name"] == feature, "weight"].values[0]
+            for feature in df_params["name"]
+        )
 
     # Вычисление целевой переменной и добавление в датафрейм
-    df_synthetic['target'] = df_synthetic.apply(calc_target, axis=1)
-    
+    df_synthetic["target"] = df_synthetic.apply(calc_target, axis=1)
+
     return df_synthetic
 
 

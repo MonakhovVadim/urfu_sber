034dc82bbfe33c19dfc467459cc497ecbc062e83
tmerurfu <rainwalker17@mail.ru>
2024-12-20 00:57:54 +0400

    fix test

--- tests/test_load_dataset.py
+++ tests/test_load_dataset.py
@@ -3,12 +3,15 @@
 # №№ Тест проверяет, что данные возвращаются как DataFrame и содержат ожидаемые столбцы.
 
 import pandas as pd
-from urfu_sber.src.common_functions import load_dataset, DATA_TYPE
+from urfu_sber.src.common_functions import load_dataset, DATA_TYPE, MODEL_TYPE
+
+from urfu_sber.tests.prepare_test_data import prepare_test_data
 
 
 def test_load_dataset_valid():
+    prepare_test_data()
     # Загружаем данные TRAIN
-    data = load_dataset(DATA_TYPE.TRAIN, "DEFAULT")
+    data = load_dataset(DATA_TYPE.TRAIN, MODEL_TYPE.DEFAULT)
     assert isinstance(
         data, pd.DataFrame
     ), "Данные должны быть представлены как DataFrame"
